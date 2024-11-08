79e468f895287368abab20dd3932dc562656f128
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-08 15:27:01 +0400

    add number function in c

--- app.c
+++ app.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+int addNumbers(int number1, int number2) {
+   return number1 + number2;
+
+}
+
 int main() {
    
-
+    int sum = addNumbers(2, 5);
+    printf("Result = %d", sum);
     return 0;
 
 }
