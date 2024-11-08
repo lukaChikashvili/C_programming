73c32d4e45501730e9dee36bf66bbfef86540c5c
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-08 16:08:28 +0400

    add string methods

--- app.c
+++ app.c
@@ -4,12 +4,32 @@
 
 
 int main() {
-    
+    // string length
     char language[] = "chinese";
-
+    
     printf("%zu", strlen(language));
 
+    // string copy
+     char secondLanguage[strlen(language)];
 
-    return 0;
+     strcpy(secondLanguage, language);
+     printf("\n second language: %s", secondLanguage);
+
+     // string concatanation
+     char name[] = "luka ";
+     char surname[] = "chikashvili";
+
+     strcat(name, surname);
+     printf("\n %s", name);
+
+     // string compare
+     char str1[] = "abc";
+     char str2[] = "abc";
+
+     int result = strcmp(str1, str2);
+     printf("\n%d", result);
+
+     
+         return 0;
 
 }
